b4412938989eb74dec749fb75fe7f51e52c86107
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-01-10 17:44:51 +0100

    microsoft-graph: adding a fake usage of the microsoft-graph base layer

--- microsoft-graph/example.go
+++ microsoft-graph/example.go
@@ -1,4 +1,8 @@
 package microsoft_graph
 
+import (
+	_ "github.com/tombuildsstuff/go-azure-sdk/sdk/client/msgraph"
+)
+
 // Example is a placeholder value just so this is a valid Go module
 const Example = "Value"
